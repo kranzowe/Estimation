9f71b329d4010b22f406e9fe79c4e12940d2a341
Bill Hsu <bill10361239@gmail.com>
2026-04-04 17:54:00 -0700

    Localization

--- estimation_localization/launch/amcl_localization.launch.py
+++ estimation_localization/launch/amcl_localization.launch.py
@@ -3,16 +3,68 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription, LogInfo, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
-def generate_launch_description():
-    pkg_share = get_package_share_directory('estimation_localization')
 
-    default_map = os.path.join(pkg_share, 'maps', 'basement.yaml')
-    amcl_params = os.path.join(pkg_share, 'config', 'amcl.yaml')
-    lifecycle_params = os.path.join(pkg_share, 'config', 'lifecycle_localization.yaml')
+def generate_launch_description():
+    localization_pkg = get_package_share_directory('estimation_localization')
+    mapping_pkg = get_package_share_directory('estimation_mapping')
+
+    default_map = os.path.join(localization_pkg, 'maps', 'lab.yaml')
+    amcl_params = os.path.join(localization_pkg, 'config', 'amcl.yaml')
+    lifecycle_params = os.path.join(localization_pkg, 'config', 'lifecycle_localization.yaml')
+    urdf_file = os.path.join(mapping_pkg, 'urdf', 'simple.urdf')
+    rplidar_launch = os.path.join(
+        get_package_share_directory('rplidar_ros'),
+        'launch',
+        'rplidar_a1_launch.py',
+    )
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
 
     map_yaml = LaunchConfiguration('map')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    rplidar_launch_action = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(rplidar_launch)
+    )
+
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{
+            'yaml_filename': map_yaml,
+            'use_sim_time': False
+        }]
+    )
+
+    amcl_node = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        parameters=[amcl_params]
+    )
+
+    lifecycle_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
+        parameters=[lifecycle_params]
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -20,31 +72,9 @@ def generate_launch_description():
             default_value=default_map,
             description='Full path to map yaml file'
         ),
-
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[{
-                'yaml_filename': map_yaml,
-                'use_sim_time': False
-            }]
-        ),
-
-        Node(
-            package='nav2_amcl',
-            executable='amcl',
-            name='amcl',
-            output='screen',
-            parameters=[amcl_params]
-        ),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_localization',
-            output='screen',
-            parameters=[lifecycle_params]
-        ),
+        robot_state_publisher_node,
+        map_server_node,
+        amcl_node,
+        lifecycle_node,
+        rplidar_launch_action,
     ])
--- estimation_localization/launch/slam_localization.launch.py
+++ estimation_localization/launch/slam_localization.launch.py
@@ -1,11 +1,37 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription, LogInfo, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
+
 def generate_launch_description():
-    pkg_share = get_package_share_directory('estimation_localization')
-    slam_params = os.path.join(pkg_share, 'config', 'slam_localization.yaml')
+    localization_pkg = get_package_share_directory('estimation_localization')
+    mapping_pkg = get_package_share_directory('estimation_mapping')
+
+    slam_params = os.path.join(localization_pkg, 'config', 'slam_localization.yaml')
+    urdf_file = os.path.join(mapping_pkg, 'urdf', 'simple.urdf')
+    rplidar_launch = os.path.join(
+        get_package_share_directory('rplidar_ros'),
+        'launch',
+        'rplidar_a1_launch.py',
+    )
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    rplidar_launch_action = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rplidar_launch)
+    )
 
     slam_node = Node(
         package='slam_toolbox',
@@ -16,5 +42,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        robot_state_publisher_node,
+        rplidar_launch_action,
         slam_node
     ])
