97da213c941d29eb0c46efbcfb36b5e8d0382d46
root <vpietropaolo1998@gmail.com>
2026-04-24 16:26:52 +0000

    Launch lidar

--- pf_localization/launch/pf_foxglove.launch.py
+++ pf_localization/launch/pf_foxglove.launch.py
@@ -1,7 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
 
 def generate_launch_description():
+    rplidar_launch = os.path.join(
+        get_package_share_directory('rplidar_ros'),
+        'launch',
+        'rplidar_a1_launch.py',
+    )
+    rplidar_launch_action = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rplidar_launch),
+    )
+
     return LaunchDescription([
                 Node(
             package="pf_localization",
@@ -22,4 +35,5 @@ def generate_launch_description():
                 "max_qos_depth": 10,
             }],
         ),
+        rplidar_launch_action,
     ])
--- pf_localization/launch/pf_headless.launch.py
+++ pf_localization/launch/pf_headless.launch.py
@@ -1,7 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
 
 def generate_launch_description():
+    rplidar_launch = os.path.join(
+        get_package_share_directory('rplidar_ros'),
+        'launch',
+        'rplidar_a1_launch.py',
+    )
+    rplidar_launch_action = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rplidar_launch),
+    )
     return LaunchDescription([
                 Node(
             package="pf_localization",
@@ -9,4 +21,5 @@ def generate_launch_description():
             name="pf_localization",
             parameters=[{"debug": False}]
         ),
+        rplidar_launch_action,
     ])
