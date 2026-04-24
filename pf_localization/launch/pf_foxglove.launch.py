813f35a0c2bd06d3d42143e5d8aa51c5067321d0
root <vpietropaolo1998@gmail.com>
2026-04-24 18:49:05 +0000

    Launch arg params

--- pf_localization/launch/pf_foxglove.launch.py
+++ pf_localization/launch/pf_foxglove.launch.py
@@ -3,6 +3,8 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
@@ -15,12 +17,26 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(rplidar_launch),
     )
 
+    ARG_PARAM_lidar_resolution = DeclareLaunchArgument(
+        'lidar_resolution',
+        default_value=60,
+    )
+    ARG_PARAM_num_particles = DeclareLaunchArgument(
+        'num_particles',
+        default_value=100,
+    )
+
     return LaunchDescription([
-                Node(
+        ARG_PARAM_lidar_resolution,
+        ARG_PARAM_num_particles,
+        Node(
             package="pf_localization",
             executable="pf_node.py",
             name="pf_localization",
-            parameters=[{"debug": True}]
+            parameters=[{
+                "debug": True,
+                'num_particles': LaunchConfiguration('num_particles'),
+                'lidar_resolution': LaunchConfiguration('lidar_resolution')}]
         ),
 
         Node(
