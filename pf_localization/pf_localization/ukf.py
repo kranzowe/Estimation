6e531cc130fc9ddddbbd289fc246ce0f1d6d0c60
root <vpietropaolo1998@gmail.com>
2026-04-24 20:45:27 +0000

    cov

--- pf_localization/pf_localization/ukf.py
+++ pf_localization/pf_localization/ukf.py
@@ -48,8 +48,8 @@ class UKF:
             offset = np.sqrt(self.n + self.l) * S[:,i]
             sp.append(self.x + offset)
             sp.append(self.x - offset)
-        innov_cov = np.diag(h(sp[0])[1]).copy()
-        logger.warn(f"{innov_cov.shape}")
+        logger.warn(f"{h(self.x)}")
+        innov_cov = np.diag(h(self.x)[1]).copy()
         spp = [h(p)[0] for p in sp]
         yhat = np.sum([wm * p for wm, p in zip(self.wm, spp)], axis=0)
         cross_cov = np.zeros((self.n, y.shape[0]))
